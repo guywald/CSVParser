8c3d2c8c11419bec6d17a48519e13da7b3b87189
Yuval Itzchakov <yuvalos@gmail.com>
2014-10-28 20:13:44 +0200

    Added the missing CsvParserUnitTests file

---
+++ CsvParser/CsvParser.Tests/CsvParserUnitTests.cs
@@ -0,0 +1,114 @@
+﻿using CsvParser.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace CsvParser.Tests
+{
+    [TestClass]
+    public class CsvParserUnitTests
+    {
+
+        [TestMethod]
+        public void CsvParser_LineCountEquality_Calculated()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser(@"D:\TestCsvFile.csv");
+            Assert.AreEqual(5, parser.Count, "There aren't 5 lines");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void CsvParser_PathDosentExist_FileNotFoundException()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser(@"NotFound.csv");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CsvParser_EmptyCsvFileName_ArgumentNullException()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CsvParser_NullCsvFileName_ArgumentNullException()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser(null);
+        }
+
+        [TestMethod]
+        public void CsvParser_ShouldParseFifthRow()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser(@"D:\TestCsvFile.csv");
+            dynamic fifthLine = parser[4];
+            Assert.AreEqual(fifthLine.time, "18:04");
+            Assert.AreEqual(fifthLine.status, "500");
+            Assert.AreEqual(fifthLine.result, "Internal Server Error");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void CsvParser_IndexIsOutOfRange_IndexOutOfRangeException()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser(@"D:\TestCsvFile.csv");
+            dynamic tenthLine = parser[10];
+        }
+
+        [TestMethod]
+        public void CsvParser_IterateUsingIndexer()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser(@"D:\TenThounsandRowCsv.csv");
+            var sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < parser.Count; i++)
+            {
+                dynamic row = parser[i];
+                Console.WriteLine("Line Count: {0} - {1}: {2}", i, row.time, row.status);
+            }
+
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
+
+            Assert.AreEqual(parser.Count, 9999);
+
+        }
+
+        [TestMethod]
+        public void CsvParser_IterateUsingEnumerator()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser(@"D:\TenThounsandRowCsv.csv");
+            int index = 0;
+            var sw = new Stopwatch();
+            sw.Start();
+            foreach (var row in parser)
+            {
+                Debug.WriteLine("Index: {0} - {1}: {2}", index, row["time"], row["status"]);
+                index++;
+            }
+            sw.Stop();
+            Debug.WriteLine(sw.Elapsed);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CsvParser_AccessViaOutOfRangeIndexer_ArgumentOutOfRangeException()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser(@"D:\TenThounsandRowCsv.csv");
+            dynamic row = parser[int.MaxValue];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (CsvParseException))]
+        public void CsvParser_MoreHeadersThanRows_CsvParseException()
+        {
+            Parser.CsvParser parser = new Parser.CsvParser(@"D:\CsvEmptyLine.csv");
+            foreach (var row in parser)
+            {
+                Debug.WriteLine(row["time"]);
+            }
+        }
+    }
+}
